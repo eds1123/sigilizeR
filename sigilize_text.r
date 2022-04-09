674821947e871a0056b75d06261fea0ee40eff8f
eddieTomo <42616940+eddieTomo@users.noreply.github.com>
2022-04-09 17:04:58 +0100

    change to use R native pipe

--- sigilize_text.r
+++ sigilize_text.r
@@ -13,28 +13,32 @@
 #
 # --------------------------------------------- #
 
-library(magrittr)  # Function uses pipe
 library(stringr)  # To process text
 library(runes)  # For conversion to Nordic runes when specified.
-#library (???)  # For validating input
+
+# TODO Validate Input using regex
+  # [^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+ is the regex for emails
+  # ^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$ for phones
+  # https://statisticsglobe.com/r-test-if-character-is-in-string#example-2-check-if-string-contains-character-using-str-detect-function-of-stringr-package for general strings (basically grepl() or str_detect() )
+  # regex [0-9] for numbers
 
 # Base function
 sigilize_text <- function(input_string, convert = NULL) {
-  
+
   # first validate input
   if (is.character(input_string)) {
     # remove vowels and convert to vector
-    split_text <- input_string %>%
-      str_remove_all("[aeiou]") %>%
+    split_text <- input_string |>
+      str_remove_all("[aeiou]") |>
       strsplit("")
     # only keep unique letters and remove spaces
-    sigil_text <- unique(split_text[[1]]) %>%
-      str_extract("[A-z]") %>%
-      .[!is.na(.)] %>%
+    sigil_text <- unique(split_text[[1]]) |>
+      str_extract("[A-z]") |>
+      .[!is.na(.)] |>
       # randomize order of characters
-      sample() %>%
+      sample() |>
       # concatenate as uppercase
-      str_to_upper() 
+      str_to_upper()
     # parse convert argument
     if (is.null(convert)) {
       # normal text output
@@ -45,9 +49,9 @@ sigilize_text <- function(input_string, convert = NULL) {
     }
     return(sigil_output)
   }
-  
+
 }
-  
+
 
 else if (convert == "numbers") {
   sigil_output <- sigil_text
@@ -61,23 +65,23 @@ else if (convert == "numbers") {
     } else {
       sigil_output <- sigil_text
       return(sigil_output)
-    } 
-  
+    }
+
   # if input is not string, throw error message
   } else {
     return("Input must be a text string")
   }
-  
+
 }
 
 test_string = "some text to test"
-split_text <- test_string %>%
-  str_remove_all("[aeiou]") %>%
+split_text <- test_string |>
+  str_remove_all("[aeiou]") |>
   strsplit("")
-sigil_text <- unique(split_text[[1]]) %>%
-  str_extract("[A-z]") %>%
+sigil_text <- unique(split_text[[1]]) |>
+  str_extract("[A-z]") |>
   # randomize order of characters
-  sample() %>%
+  sample() |>
   # concatenate as uppercase
-  str_to_upper() %>%
+  str_to_upper() |>
   paste(collapse ='')
